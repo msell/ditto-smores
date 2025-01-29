f5ded306473d605d83e9317fc7256bf47937adfc
Matt Sell <matt@atomicbytes.com>
2025-01-29 14:57:30 -0600

    wip

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,32 +1,47 @@
 import React, { useState, useEffect, useRef } from 'react'
 import {
   FlatList,
-  Text,
-  StyleSheet,
   PermissionsAndroid,
   Platform,
   View,
-  TextInput,
-  Button,
-  SafeAreaView,
+  Pressable,
   Alert,
+  ViewStyle,
+  TextStyle,
 } from 'react-native'
 import {
   Ditto,
   IdentityOnlinePlayground,
   TransportConfig,
 } from '@dittolive/ditto'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons'
+import Animated, { FadeInUp, FadeOutDown } from 'react-native-reanimated'
+import { Button, Screen, TextField, Text } from '@/components'
+import { useAppTheme } from '@/utils/useAppTheme'
+import { ThemedStyle } from '@/theme'
+import * as Crypto from 'expo-crypto'
 
 type Task = {
   id: string
   title: string
   completed: boolean
 }
+
+function generateUUID(): string {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+    const r = (Math.random() * 16) | 0
+    const v = c === 'x' ? r : (r & 0x3) | 0x8
+    return v.toString(16)
+  })
+}
+
 export default function WelcomeScreen() {
+  const { themed } = useAppTheme()
   const [task, setTask] = useState<string>('')
   const [tasks, setTasks] = useState<Task[]>([])
-
+  const [timeouts, setTimeouts] = useState<Record<string, NodeJS.Timeout>>({})
   const ditto = useRef<Ditto | null>(null)
+  const textFieldRef = useRef<any>(null)
 
   async function requestPermissions() {
     const permissions = [
@@ -100,71 +115,193 @@ export default function WelcomeScreen() {
   }, [])
 
   async function handleAddTask() {
-    if (ditto.current === null) {
+    if (ditto.current === null || task.trim().length === 0) {
+      Alert.alert('Error', `TASK ${task}`)
       return
     }
 
-    if (task.trim().length === 0) {
-      return
-    }
-
-    const result = await ditto.current.store.execute(
-      `INSERT INTO tasks DOCUMENTS ({ 'title': '${task}' })`
+    const newId = Crypto.randomUUID()
+    const newTask = { id: newId, title: task, completed: false }
+    await ditto.current.store.execute(
+      `INSERT INTO tasks DOCUMENTS (:document) ON ID CONFLICT DO UPDATE`,
+      { document: newTask }
     )
-    const newId = result.mutatedDocumentIDs().map((id) => id.value)[0]
-
-    const newTask: Task = {
-      title: task,
-      id: newId,
-      completed: false,
-    }
-    setTasks((currentTasks) => [...currentTasks, newTask])
     setTask('')
+    textFieldRef.current?.focus()
+  }
+
+  const toggleTaskCompletion = async (taskId: string, isCompleted: boolean) => {
+    if (ditto.current === null) return
+
+    await ditto.current.store.execute(
+      `UPDATE tasks SET completed = ${isCompleted} WHERE id = '${taskId}'`
+    )
+
+    if (isCompleted) {
+      const timeoutId = setTimeout(async () => {
+        await ditto.current?.store.execute(
+          `DELETE FROM tasks WHERE id = '${taskId}'`
+        )
+      }, 5000)
+
+      setTimeouts((prevTimeouts) => ({
+        ...prevTimeouts,
+        [taskId]: timeoutId,
+      }))
+    } else {
+      if (timeouts[taskId]) {
+        clearTimeout(timeouts[taskId])
+        setTimeouts((prevTimeouts) => {
+          const { [taskId]: _, ...rest } = prevTimeouts
+          return rest
+        })
+      }
+    }
   }
 
   const renderItem = ({ item }: { item: Task }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{item.title}</Text>
-    </View>
+    <Animated.View
+      entering={FadeInUp}
+      exiting={FadeOutDown}
+      style={themed($card)}
+    >
+      <Pressable onPress={() => toggleTaskCompletion(item.id, !item.completed)}>
+        <View style={themed($row)}>
+          {item.completed ? (
+            <MaterialIcons
+              name="check"
+              style={themed($taskIcon(item.completed))}
+            />
+          ) : (
+            <MaterialIcons
+              name="check-box-outline-blank"
+              style={themed($taskIcon(item.completed))}
+            />
+          )}
+          <Text style={themed($taskTitle(item.completed))}>{item.title}</Text>
+        </View>
+      </Pressable>
+    </Animated.View>
   )
 
   return (
-    <SafeAreaView style={styles.container}>
-      <TextInput
-        style={styles.input}
-        placeholder="Enter a task"
-        value={task}
-        onChangeText={setTask}
-      />
-      <Button title="Add Task" onPress={handleAddTask} />
-      <FlatList
-        data={tasks}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    </SafeAreaView>
+    <Screen safeAreaEdges={['top']} contentContainerStyle={themed($container)}>
+      <View style={themed($topContainer)}>
+        <Text style={themed($welcomeHeading)} preset="heading">
+          Check List
+        </Text>
+        <View style={themed($contentContainer)}>
+          <View style={themed($inputRow)}>
+            <TextField
+              ref={textFieldRef}
+              autoFocus
+              placeholder="Add a task"
+              value={task}
+              onChangeText={setTask}
+              containerStyle={themed($input)}
+              style={themed($textField)}
+              onSubmitEditing={handleAddTask}
+            />
+            <Button
+              preset="filled"
+              onPress={handleAddTask}
+              style={themed($addButton)}
+            >
+              Add
+            </Button>
+          </View>
+          <FlatList
+            data={tasks}
+            renderItem={renderItem}
+            keyExtractor={(item) => item.id}
+            style={themed($taskList)}
+          />
+        </View>
+      </View>
+    </Screen>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 20,
-    backgroundColor: '#fff',
-  },
-  input: {
-    marginBottom: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-  },
-  item: {
-    padding: 10,
-    marginVertical: 8,
-    backgroundColor: '#f9c2ff',
-    borderRadius: 5,
-  },
-  title: {
-    fontSize: 18,
-  },
+const $row: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: spacing.sm,
+  width: '100%',
+})
+
+const $inputRow: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  flexDirection: 'row',
+  alignItems: 'center',
+  gap: spacing.sm,
+  width: '100%',
+  justifyContent: 'space-between',
+})
+
+const $input: ThemedStyle<ViewStyle> = () => ({
+  flexGrow: 1,
+})
+
+const $textField: ThemedStyle<TextStyle> = () => ({
+  height: 40,
+})
+
+const $addButton: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  paddingHorizontal: spacing.lg,
+})
+
+const $taskTitle =
+  (completed: boolean): ThemedStyle<TextStyle> =>
+  ({ colors }) => ({
+    textDecorationLine: completed ? 'line-through' : 'none',
+    color: colors.palette.neutral800,
+  })
+
+const $taskIcon =
+  (completed: boolean): ThemedStyle<TextStyle> =>
+  ({ colors }) => ({
+    color: completed ? colors.palette.primary500 : colors.palette.neutral400,
+    fontSize: 24,
+  })
+
+const $container: ThemedStyle<ViewStyle> = ({ colors }) => ({
+  flex: 1,
+  backgroundColor: colors.background,
+})
+
+const $topContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  flex: 1,
+  alignItems: 'center',
+  paddingTop: spacing.xl,
+  width: '100%',
+})
+
+const $welcomeHeading: ThemedStyle<TextStyle> = ({ spacing }) => ({
+  marginBottom: spacing.md,
+})
+
+const $taskList: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  flexGrow: 1,
+  width: '100%',
+  paddingTop: spacing.lg,
+  gap: spacing.xs,
+})
+
+const $contentContainer: ThemedStyle<ViewStyle> = ({ spacing, colors }) => ({
+  flex: 1,
+  width: '100%',
+  maxWidth: 600,
+  gap: spacing.lg,
+  paddingHorizontal: spacing.lg,
+  backgroundColor: colors.palette.accent200,
+  borderRadius: 10,
+  padding: spacing.lg,
+  boxShadow: `0 0 10px ${colors.palette.neutral400}`,
+})
+
+const $card: ThemedStyle<ViewStyle> = ({ colors, spacing }) => ({
+  backgroundColor: colors.palette.neutral100,
+  borderRadius: 8,
+  padding: spacing.md,
+  marginVertical: spacing.xs,
+  boxShadow: `0 2px 4px ${colors.palette.neutral400}`,
 })
